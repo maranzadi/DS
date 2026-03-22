3afcb0eb5bcea78abdd813a3b53fc9e2d35cd375
Manex Aranzadi <manexae@protonmail.com>
2026-03-22 22:33:07 +0100

    ahora se deberia de guardar en la variable

--- server.sh
+++ server.sh
@@ -19,7 +19,7 @@ trap 'echo "Matando servidor $SERVER_PID"; kill $SERVER_PID 2>/dev/null' EXIT
 
 while true; do
   
-  newHash=  find $carpeta -type f -print0 | sort -z | xargs -0 shasum -a 256 | shasum -a 256
+  newHash=$(find $carpeta -type f -print0 | sort -z | xargs -0 shasum -a 256 | shasum -a 256)
   if [[ hash !=newHash ]]; then
     
     rm -rf $carpetaPrincital/$carpeta
