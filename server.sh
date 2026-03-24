90107748ca64a3fbb3f8df5384099267f056f617
Manex Aranzadi <manexae@protonmail.com>
2026-03-24 18:50:33 +0100

    konponduta scripta

--- server.sh
+++ server.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
 carpeta=$1
-carpetaPrincital=//home/user/ds
+carpetaPrincital=/home/user/ds
 hash=1
-
 
 dowload() {
   echo "server montado"
-  cp -r $carpeta $carpetaPrincital
+  cp -r "/home/user/SharedFiles/$carpeta" $carpetaPrincital/
   cd $carpetaPrincital/$carpeta
   make clean
   make
-  python -m SimpleHTTPServer 8000 > /dev/null 2>&1
+  python -m SimpleHTTPServer 8000 >/dev/null 2>&1
 }
 
 dowload &
 PID=$!
-trap 'echo "Matando servidor $PID"; kill $SERVER_PID 2>/dev/null' EXIT
-
+trap 'echo "Matando servidor $PID"; kill $PID 2>/dev/null' EXIT
 
 while true; do
-  
-  newHash=$(find $carpeta -type f -print0 | sort -z | xargs -0 shasum -a 256 | shasum -a 256)
-  if [[ hash !=newHash ]]; then
+
+  newHash=$(find "$carpetaPrincital/$carpeta" -type f -print0 | sort -z | xargs -0 shasum -a 256 | shasum -a 256)
+  if [[ $hash != $newHash ]]; then
 
     hash=$newHash
-    rm -rf $carpetaPrincital/$carpeta
+    rm -rf "$carpetaPrincital/$carpeta"
 
     actualPWD=$(pwd)
 
-    cp -r $carpeta $carpetaPrincital
-    cd $carpetaPrincital/$carpeta
+    cp -r "/home/user/SharedFiles/$carpeta" $carpetaPrincital
+    cd "$carpetaPrincital/$carpeta"
     echo "------------------------------"
+    temp=$(pwd)
+    echo $temp
     make clean
     make
 
     cd $actualPWD
-    
+
   fi
   sleep 5
 done
-
