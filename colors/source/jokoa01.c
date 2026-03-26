b2151a451dcb7aea01fdb986199ec6beb5872559
Manex Aranzadi <manexae@protonmail.com>
2026-03-26 20:59:52 +0100

    no se de dode coño sale esa linea

--- colors/source/jokoa01.c
+++ colors/source/jokoa01.c
@@ -42,12 +42,12 @@ void jokoa01()
 
 	swiWaitForVBlank();
 
-	float tickS =0.5;
-	int bitX =1024;
-	int latch = 65.536 - (1/tickS)*33554432/bitX; //latc=0
-	DENB0_DAT = latch;
-	DENB0_KNT = 0x00C3; //1024
-	irqSet(IRQ_TIMER0 , denbZerbErr)
+	// float tickS =0.5;
+	// int bitX =1024;
+	// int latch = 65.536 - (1/tickS)*33554432/bitX; //latc=0
+	// DENB0_DAT = latch;
+	// DENB0_KNT = 0x00C3; //1024
+	// irqSet(IRQ_TIMER0 , denbZerbErr)
 	
 	
 	//iprintf("\x1b[22;5HHau idazte proba bat da");	// Hau 22 lerroan eta 5 zutabean hasiko da idazten.
@@ -83,20 +83,20 @@ void jokoa01()
 
 			//consoleClear();
 			tekla=SakatutakoTekla();
-			iprintf("\x1b[1;1H\033[K");
-			iprintf("\x1b[1;1HAldagai. Balioa=%s", lista[tekla]);
+			//iprintf("\x1b[1;1H\033[K");
+			iprintf("\x1b[1;1HTekla=%s", lista[tekla]);
 			//iprintf("\x1b[23;5HTekla sakatuta. Balioa=%c", lista[tekla]);
 			// iprintf("\x1b[1;1HAldagai. Balioa=%s", lista[tekla]);
 
-			if (tekla=R)
+			if (tekla==R)
 			{
 				kolorea++;
-			}else if (tekla=L)
+			}else if (tekla==L)
 			{
 				kolorea++;
 			}
 			
-			if(tekla=START){
+			if(tekla==START){
 				memset(VRAM_A, 0, 256 * 192 * 2);
 			}
 
@@ -108,12 +108,12 @@ void jokoa01()
 				kolorea=2;
 			}
 
-			if (tekla=GORA)
+			if (tekla==GORA)
 			{
-				rgbKol[kolorea]+=2;
-			}else if (tekla=BEHERA)
+				rgbKol[kolorea]+=1;
+			}else if (tekla==BEHERA)
 			{
-				rgbKol[kolorea]-=2;
+				rgbKol[kolorea]-=1;
 			}
 			if (rgbKol[kolorea]<0)
 			{
@@ -124,25 +124,25 @@ void jokoa01()
 			}
 			color = RGB15(rgbKol[0], rgbKol[1], rgbKol[2]);
 
-			iprintf("\x1b[1;1H\033[K");
+			//iprintf("\x1b[5;1H\033[K");
 			iprintf("\x1b[5;1Hrgb=%d, %d, %d", rgbKol[0], rgbKol[1], rgbKol[2]);
 			iprintf("\x1b[7;1HKolorea: %d", kolorea);
 
 			//iprintf("\x1b[50;5HTekla sakatuta. Balioa=%d", tekla);
 		}else{
 			
-			iprintf("\x1b[1;1HAldagai. Balioa=null");
+			iprintf("\x1b[1;1HTekla=null");
 		}
 
 		if (ukimenUkitua()==1)
 		{
 			pos_pantaila = ukimenPos(); 
 
-			size_t i;
-			for (i = 0; i < 3; i++)
-			{
-				iprintf("\x1b[%d;1H\033[K", i+12);
-			}
+			// size_t i;
+			// for (i = 0; i < 3; i++)
+			// {
+			// 	iprintf("\x1b[%d;1H\033[K", i+12);
+			// }
 			
 
 			int x= pos_pantaila.px;
@@ -150,7 +150,7 @@ void jokoa01()
 			float pres= presioa(pos_pantaila.z1, pos_pantaila.z2, x);
 			iprintf("\x1b[12;1HposX=%d, posY=%d", x, y);
 			iprintf("\x1b[13;1HRaw: %04X, %04X", pos_pantaila.z1, pos_pantaila.z2);
-			iprintf("\x1b[14;1HPres: %f", pres);
+			iprintf("\x1b[14;1HPres: %d", (int)pres);
 
 			
 			float minP = 100;   // presión fuerte
@@ -168,22 +168,21 @@ void jokoa01()
 			// Escalar a radio
 			int radius = 1 + t * 20;
 			drawCircle(x, y, radius, color);
-			iprintf("\x1b[16;1HRadius: %d", radius);
+			iprintf("\x1b[16;1HRadius: %d", (int)t);
 
 
-			iprintf("\x1b[1;18H\033[K");
-			iprintf("\x1b[1;18HAldagai. rgbKol=%d, %d, %d", rgbKol[0], rgbKol[1], rgbKol[2]);
+	
 
 		}else{
-			size_t i;
-			for (i = 0; i < 2*2; i=i+2)
-			{
-				iprintf("\x1b[%d;1H\033[K", i+12);
+			// size_t i;
+			// for (i = 0; i < 2*2; i=i+2)
+			// {
+			// 	iprintf("\x1b[%d;1H\033[K", i+12);
 			
-			}
+			// }
 
-			iprintf("\x1b[12;1HposX=%d, posY=%d", 0, 0);
-			iprintf("\x1b[13;1HRaw: %f, %f", 0, 0);
+			iprintf("\x1b[12;1HposX=%04X, posY=%04X", 0, 0);
+			iprintf("\x1b[13;1HRaw: %04X, %04X", 0, 0);
 
 		}
 		
