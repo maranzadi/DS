bbd3dbea9ff3efe25a2fd33c4bc678f45564b5a6
Manex Aranzadi <manexae@protonmail.com>
2026-03-31 21:24:49 +0200

    funtzionatzen du

--- colors/source/main.c
+++ colors/source/main.c
@@ -16,32 +16,58 @@ adibide batean oinarrituta.
 #include "definizioak.h"
 #include "spriteak.h"
 #include "jokoa01.h"
+#include "periferikoak.h"
+
+
+//https://www.pmallory.com/bitmap-graphics-on-the-nintendo-ds-part-1-framebuffer-mode.html
+#define OFFSET(r,c,w) ((r)*(w)+(c))
+#define SCANLINECOUNTER   *(vu16 *)0x4000006
+#define SCREENHEIGHT (192)
+#define SCREENWIDTH  (256)
+
+#define COLOR(r,g,b)  ((r) | (g)<<5 | (b)<<10)
+#define REG_DISPCNT_MAIN  (*(vu32*)0x04000000)
+#define REG_DISPCNT_SUB   (*(vu32*)0x04001000)
+#define MODE_FB0          (0x00020000)
+#define VRAM_A            ((u16*)0x6800000)
+#define VRAM_A_CR         (*(vu8*)0x04000240)
+#define VRAM_ENABLE       (1<<7)
+#define SCANLINECOUNTER   *(vu16 *)0x4000006
 
 int bgId;
 u16* framebuffer;
 //---------------------------------------------------------------------------------
-int main(void) {
-//---------------------------------------------------------------------------------
-
-	//HasieratuGrafikoakSpriteak();
-	consoleDemoInit();
+void setPixel(int row, int col, u16 color);
+void waitForVblank();
 
 
-	videoSetMode(MODE_5_2D);
-    vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+int main(void)
+{
+	REG_DISPCNT_MAIN = MODE_FB0;
+	VRAM_A_CR = VRAM_ENABLE;
 
-    // 2. Setup Background 3
-    bgId = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0, 0);
-	framebuffer = bgGetGfxPtr(bgId);
-	
-	EtenakBaimendu();
+	setPixel(100, 100, COLOR(15,27,7));
 
-	// Zuen jokoari dei egingo zaio hemendik
-	jokoa01();
+	while(1) {
+        if (ukimenUkitua()) {
+            touchPosition touch = ukimenPos();
+            
+            // Pintar un punto blanco donde tocas para probar el stylus
+            setPixel(touch.px, touch.py, COLOR(15,27,7));
 
-	return(0);
+            // El printf ahora sí encontrará la consola en la pantalla de ARRIBA
+            iprintf("\x1b[3;1HLocation: %d, %d   ", touch.px, touch.py);
+        }
 
+        //swiWaitForVBlank();
+    }
 }
-
+void setPixel(int row, int col, u16 color) {
+    VRAM_A[OFFSET(row, col, SCREENWIDTH)] = color;
+}
+void waitForVblank() {
+    while (SCANLINECOUNTER > SCREENHEIGHT);
+    while (SCANLINECOUNTER < SCREENHEIGHT);
+}
 /***********************2025-2026*******************************/
 
